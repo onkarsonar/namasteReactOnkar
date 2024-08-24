97453343f199fd37a5b77625d9ad5954af0affbb
onkarsonar2020 <onkarsonar2020@gmail.com>
2024-08-24 23:13:00 +0530

    hook react

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import {LOGO_URL} from "../utils/constants";
+
+const Header = () => {
+    return (
+      <div className="header">
+        <div className="logo-container">
+          <br></br>
+          &nbsp;<img src={LOGO_URL} className="logo" />
+  
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+  export default Header;
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,g_auto,w_588,h_320,c_fill/";
+
+export const LOGO_URL = "https://cdn.dribbble.com/users/630677/screenshots/3833541/media/201454f743f48c415a38c49419275692.jpg?resize=400x0";
+

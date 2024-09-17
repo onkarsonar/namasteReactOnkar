788dcb11d579a963fdee443b62b59b731d6902b8
onkarsonar2020 <onkarsonar2020@gmail.com>
2024-09-17 22:48:46 +0530

    use effect added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,33 +1,50 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import  resList  from "../utils/mockData";
+import { useState, useEffect } from "react";
+import resList from "../utils/mockData";
 
 
 const Body = () => {
-    const [listOfRestaurants,setListOfRestaurants] = useState(resList);
-    return (
-      <div className="body">
-        <div className="filter">
-            <button className="filter-btn" onClick={()=>{
-                const filteredList = listOfRestaurants.filter(
-                    (res)=>res.data.avgRating>4
-                );
-                setListOfRestaurants(filteredList);
-            }}>To rated restaurant</button>
-        </div>
-        <div className="res-container">
-      
-          {
-            listOfRestaurants.map((restaurant) => (
-              <RestaurantCard key={restaurant.data.id} resData={restaurant} />)
-            )
-          }
- 
-  
-  
-        </div>
-      </div>
-    )
+  const [listOfRestaurants, setListOfRestaurants] = useState('');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.6804639&lng=74.018261&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json = await data.json();
+    // console.log(json.data.cards[2]);
+    // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+
+    setListOfRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
   }
 
-  export default Body;
+  return (
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {
+          const filteredList = listOfRestaurants.filter(
+            (res) => res.info.avgRating > 4
+          );
+          setListOfRestaurants(filteredList);
+        }}>To rated restaurant</button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants?.length > 0 &&
+          listOfRestaurants?.map((restaurants) => (
+      
+            <RestaurantCard key={restaurants?.info?.id} resData={restaurants} />
+          )
+        )
+        }
+
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Body;
